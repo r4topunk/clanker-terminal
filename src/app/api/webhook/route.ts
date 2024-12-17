4ab3d00860d8ac6cd8676ca0c542b48832b96e32
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-17 00:09:22 -0300

    feat: add dicord lib

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,26 @@
+import { config } from "@/lib/env";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+
+export const dynamic = "force-dynamic";
+
+const PROD_CHANNEL_ID = "1318413688465653881";
+const DEV_CHANNEL_ID = "1318414177294880788";
+const CHANNEL_ID =
+  process.env.NODE_ENV === "production" ? PROD_CHANNEL_ID : DEV_CHANNEL_ID;
+
+export async function POST() {
+  const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
+
+  try {
+    await rest.post(Routes.channelMessages(CHANNEL_ID), {
+      body: {
+        content: "A message via REST!",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  return Response.json({ data: "Hello World" });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
+  const handleClick = async () => {
+    const res = await fetch("/api/webhook", {
+      method: "POST",
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div className="flex gap-1 items-center justify-center min-h-screen py-2">
-      <Button>Click me</Button>
+      <Button onClick={handleClick}>Click me</Button>
       <ThemeToggle />
     </div>
   );
