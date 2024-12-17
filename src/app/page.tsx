cb1b159f6116d6e391f889eb767c0a41f1b398a1
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-17 12:16:43 -0300

    refactor: update theme toogle and add github button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   const handleClick = async () => {
@@ -10,6 +12,14 @@ export default function Home() {
   return (
     <div className="flex gap-1 items-center justify-center min-h-screen py-2">
       <Button onClick={handleClick}>Soon...</Button>
+      <Link
+        target="_blank"
+        href={"https://github.com/r4topunk/clanker-terminal"}
+      >
+        <Button size={"icon"} variant={"outline"}>
+          <GithubIcon />
+        </Button>
+      </Link>
       <ThemeToggle />
     </div>
   );
--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -14,6 +14,22 @@ import {
 export function ThemeToggle() {
   const { setTheme } = useTheme();
 
+  function switchTheme() {
+    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={switchTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
+
+export function ThemeDropdown() {
+  const { setTheme } = useTheme();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
