b297107c867d6bece91b27547b5153da554f42b3
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-16 22:35:28 -0300

    fix: remove unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
