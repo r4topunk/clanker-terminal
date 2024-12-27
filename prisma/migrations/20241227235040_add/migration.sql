1e6107ad1eefd06c86799a7e3d45380ce4cd6469
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-27 20:52:24 -0300

    feat: add indexes for castDate and neynarScore; update page to filter casts by user neynarScore

---
+++ prisma/migrations/20241227235040_add/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "Cast_castDate_idx" ON "Cast"("castDate");
+
+-- CreateIndex
+CREATE INDEX "User_neynarScore_idx" ON "User"("neynarScore");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,13 @@ export default async function Home() {
   const casts = await prisma.cast.findMany({
     include: { token: true, parent_user: true },
     orderBy: { castDate: "desc" },
-    // take: 10,
+    where: { parent_user: { neynarScore: { gte: 0.95 } } },
   });
-
-  const sortedCasts = casts
-    .sort((a, b) => {
-      const neynarDiff = b.parent_user.neynarScore - a.parent_user.neynarScore;
-      if (neynarDiff !== 0) return neynarDiff;
-      return b.parent_user.followers - a.parent_user.followers;
-    })
-    .slice(0, 333);
 
   return (
     <div className="container min-h-screen py-2 mx-auto flex flex-col">
       <Navbar />
-      <TokensTable casts={sortedCasts} />
+      <TokensTable casts={casts} />
     </div>
   );
 }
