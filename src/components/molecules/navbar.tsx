44df9a1ec2ab95bcc252ad46930573dad265f3c6
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-28 09:38:40 -0300

    refactor: link to github

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { Button } from "../ui/button";
-import { GithubIcon, Link } from "lucide-react";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 import { ThemeToggle } from "../ui/theme-toggle";
 
 function Navbar() {
   return (
     <div className="flex w-full gap-1 items-center justify-center">
       <Link
+        className={buttonVariants({ size: "icon", variant: "outline" })}
         target="_blank"
         href={"https://github.com/r4topunk/clanker-terminal"}
       >
-        <Button size={"icon"} variant={"outline"}>
-          <GithubIcon />
-        </Button>
+        <GithubIcon />
       </Link>
       <ThemeToggle />
     </div>
